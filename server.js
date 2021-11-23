790d89089dfd0802d5e4d8a8d28160a8a2a70914
Brandon Ly <bl4720@g.rit.edu>
2021-11-23 12:15:31 -0800

    Inital Push (Copy of Word-App)

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+// const API_KEY = "2acdd705-d8b4-4707-9493-41955ab453e9";
+// import path library
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+
+// for any request that doesn't match one above, send back React's index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
